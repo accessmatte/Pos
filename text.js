39b72a5b1081626ef80827170d0cb9857b3f244d
accessmatte <accessmatte@gmail.com>
2025-05-09 17:46:09 +0300

    Add files via upload

---
+++ text.js
@@ -0,0 +1,53 @@
+function terms(){
+    window.location.href = "Terms.html"
+    }
+function about(){
+   window.location.href = "About.html"
+  }
+function contact(){
+    window.location.href = "Contact.html"
+}
+function policy(){
+   window.location.href = "Policy.html"
+}
+function sitemap(){
+  window.location.href = "Sitemap.html"
+}
+function stock(){
+window.location.href = "Record.html"
+}
+function account(){
+  if (localStorage.getItem("pasword")=="" & localStorage.getItem("pasword")==null){
+    window.location.href = "Account.html"}
+    else{window.location.href = "Cart.html"}
+}
+function returns(){
+    window.location.href = "Return.html"
+}
+function home(){
+    window.location.href = "index.html"
+}
+function Service(){
+    window.location.href = "Service.html"
+}
+function cart(){
+    window.location.href = "Cart.html"
+}
+function user(){
+    window.location.href = "Cart.html"
+}
+function inst(){
+    window.location.href = "https://www.instagram.com/accessmat?igsh=YzljYTk1ODg3Zg=="
+}
+function yt(){
+    window.location.href = "https://youtube.com/@accessmat?si=dq8TFPxha1yb86zf"
+}
+function fb(){
+    window.location.href = "https://www.facebook.com/profile.php?id=61566608144644"
+}
+function wa(){
+    window.location.href = "https://wa.me/254116575820"
+}
+function shop(){
+    window.location.href = "Shop.html"
+}
